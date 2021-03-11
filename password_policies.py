63303ac14b7a3b53894f0b4d974101e4a5f6295d
nimkha <nimak47@gmail.com>
2021-03-11 16:31:07 +0100

    first commit

---
+++ password_policies.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+# This script contains different characteristics of password policies
+
+import re
+
+
+# Takes a regex and string as inputs and check if there is a match
+def check_regex_matches(regex, string):
+    if regex.search(string):
+        return True
+    else:
+        return False
+
+
+# Checks is password start with uppercase and end with digit
+def start_with_uppercase_end_with_digit(string):
+    regex = re.compile(r"^[A-Z].+\d$")
+    return check_regex_matches(regex, string)
+
+
+# Checks if password contain digit, contain lowercase and uppercase characters, and contain special character
+def contain_all_classes(string):
+    if contain_digit(string) and contain_lowercase(string) and contain_uppercase(string) and contain_special_character(string):
+        return True
+    else:
+        return False
+
+
+# Extract hash from string
+def get_hash(string):
+    match_obj = re.search(r"(.*):", string)
+    return match_obj.group(1)
+
+
+# Checks if a string contains any special characters from a set of 33 characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+def contain_special_character(string):
+    if isinstance(string, str) and len(string) > 0:
+        regex = re.compile("[\s!\"#$%&'()*+,-./:;<=>?@\[\]^_`{|}~]")
+        return check_regex_matches(regex, string)
+    else:
+        print("[-] Missing string in function 'contain_special_character'")
+        exit()
+
+
+# Checks if a string contains any digits ranging from 0-9
+def contain_digit(string):
+    regex = re.compile("[0-9]")
+    return check_regex_matches(regex, string)
+
+
+# Checks if a string contains any uppercase letters between A-Z
+def contain_uppercase(string):
+    regex = re.compile("[A-Z]")
+    return check_regex_matches(regex, string)
+
+
+# Checks if a string contains any lowercase letters between A-Z
+def contain_lowercase(string):
+    regex = re.compile("[a-z]")
+    return check_regex_matches(regex, string)
---
+++ password_policy_checker.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+
+# This script is made to calculate the percentage of passwords in a given policy
+
+import password_policies as policies
+# from charset_normalizer import CharsetNormalizerMatches as CnM
+# from charset_normalizer import detect
+
+
+# Returns file content from a file as a list using encoding="utf-8" as default
+# Be aware some files are encoded with iso-8859-15
+def get_file_content(filename, encoding="utf-8"):
+    print("[+] Getting content from file " + filename)
+
+    try:
+        with open(filename, "r", encoding=encoding) as f:
+            file_content = [line.strip() for line in f]
+        return file_content
+
+    except (UnicodeDecodeError, FileNotFoundError) as error:
+        if type(error) == UnicodeDecodeError:
+            print("[-] A unicode decode error found => " + str(error))
+        elif type(error) == FileNotFoundError:
+            print("[-] File not found")
+        else:
+            print("[-] Unknown error detected => " + str(error))
+
+
+# Removes duplicates from list one by one. This can also be done by using sets
+def remove_duplicates(list_with_duplicates):
+    print("[+] Removing duplicates")
+    new_list = []
+    counter = 0
+    for i in list_with_duplicates:
+        if i not in new_list:
+            new_list.append(i)
+            print("\r[+] Number of passwords added to list " + str(counter), end="")
+    return new_list
+
+
+# Takes two numbers and calculates the percentage of the total argument to get the result argument
+# Ex => if the total is 100 and result is 50 then the return value is 50(%)
+def find_percentage(total, result):
+    y = total / 100
+    x = result / y
+    print(
+        "[+] The percentage of the total " + "number of " + str(total) + " passwords matching policy is %.2f" % x + "%")
+    return x
+
+
+# Takes a password list and checks how many passwords matches the given policy
+def password_policy_checker(password_list):
+    print("[+] Checking passwords against policy")
+    number_of_password_matches = 0
+    for password in password_list:
+        if len(password) >= 8 and policies.start_with_uppercase_end_with_digit(password):
+            number_of_password_matches = number_of_password_matches + 1
+            print("\r[+] Number of password hits " + str(number_of_password_matches), end="")
+    print()
+    return number_of_password_matches
+
+
+# Takes a list and filename as argument writes content to file (encoding iso-8859-15 or utf-8)
+def write_list_to_file(list_to_write, filename, encoding="utf-8"):
+    counter = 0
+    with open(filename, "a", encoding=encoding) as f:
+        for line in list_to_write:
+            f.write(line)
+            f.write("\n")
+            counter = counter + 1
+            print("\r[+] Number of lines written to file " + str(counter), end="")
+    print("\n[+] Done writing list to file " + filename)
+
+
+# Prints the total length of a list
+def print_total_len_list(password_list, list_name="default value"):
+    if isinstance(password_list, list):
+        print("[+] Total number of passwords in " + list_name + " is " + str(len(password_list)))
+    else:
+        print("[-] List is empty or wrong type " + str(type(password_list)))
+
+
+# Specifically made to extract hashes downloaded from https://haveibeenpwned.com/ and writes result to .txt file
+def get_hash_from_file(file_name):
+    hashes = []
+    hits = 0
+    check_list = get_file_content(file_name)
+    for i in check_list:
+        hashes.append(policies.get_hash(i))
+        hits = hits + 1
+        print("\r[+] Number of password hits " + str(hits), end="")
+
+    write_list_to_file(hashes, file_name + "_only_hashes.txt")
+
+
+# Tries to find correct encoding for a text file and writes new file if found using utf-8
+# def normalize(text_file):
+#     try:
+#         CnM.normalize(text_file)  # should write to disk my_subtitle-***.srt
+#     except IOError as e:
+#         print('Sadly, we are unable to perform charset normalization.', str(e))
+
+# ======================================================================================
+
+if __name__ == "__main__":
+
+    print("[+] Starting program...")
+    # ==============================================================================================================
+
+    # crackstation_passwords = get_file_content("crackstation-human-only.txt")
+
+    # passwords = get_file_content("Passwords_without_duplicates/passwords_without_duplicates.txt")
+    # linkedin_passwords = get_file_content("Passwords_without_duplicates/linkedin_passwords_without_duplicates_utf8.txt")
+    # seclist_passwords = get_file_content("Passwords_without_duplicates/seclist_passwords_without_duplicates_utf8.txt")
+    # rockyou_passwords_without_duplicates = get_file_content("Passwords/RockYou/rockyou-cp1125.txt")
+
+    # rockyou_passwords = get_file_content("Passwords/RockYou/rockyou-cp1125.txt")
+    # linkedin1_passwords = get_file_content("Passwords/LinkedIn/linkedin.txt")
+    # linkedin2_passwords = get_file_content("Passwords/LinkedIn/linked2.txt")
+    # all_passwords_with_duplicates = get_file_content("all_passwords_with_duplicates.txt")
+
+    # webost_passwords = get_file_content("Passwords/SecLists/000webhost.txt")
+    # top_10_million_passwords = get_file_content("Passwords/SecLists/10-million-password-list-top-1000000.txt")
+    # gmail_passwords = get_file_content("Passwords/SecLists/alleged-gmail-passwords.txt")
+    # bt4_passwords = get_file_content("Passwords/SecLists/bt4-password.txt")
+    # darkcode_passwords = get_file_content("Passwords/SecLists/darkc0de.txt")
+    # darkweb_passwords = get_file_content("Passwords/SecLists/darkweb2017-top10000.txt")
+    # md5_passwords = get_file_content("Passwords/SecLists/md5decryptor-uk.txt")
+    # openwall_passwords = get_file_content("Passwords/SecLists/openwall.net-all.txt")
+    # xato_passwords = get_file_content("Passwords/SecLists/xato-net-10-million-passwords-1000000.txt")
+    # xato_passwords_dup = get_file_content("Passwords/SecLists/xato-net-10-million-passwords-dup.txt")
+    check_list = ["Passw0rd!", "Password1", "password", "P@ssw0rd!1"]
+
+    # ==============================================================================================================
+
+    # passwords = seclist_passwords + linkedin_passwords + rockyou_passwords_without_duplicates
+    # write_list_to_file(without, "Passwords_without_duplicates/passwords_without_duplicates_utf8.txt")
+
+    # ==============================================================================================================
+
+    passwords_matching_policy = password_policy_checker(check_list)
+    find_percentage(len(check_list), passwords_matching_policy)
+
+    # ==============================================================================================================
+
+    print("[+] Program finished...")
